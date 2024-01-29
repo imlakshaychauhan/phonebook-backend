65e340a2db0b7278da4716314a71fc97108a9582
Lakshay Chauhan <imlakshaychauhan@gmail.com>
2024-01-29 13:07:25 +0530

    Exercise - 3.14 Done

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const cors = require('cors')
+const cors = require("cors");
+const { Person } = require("./models/person");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 morgan.token("header", function (req, res) {
   return Object.keys(req.body).length === 0 ? "-" : JSON.stringify(req.body);
@@ -10,54 +13,47 @@ morgan.token("header", function (req, res) {
 // Middlewares -
 app.use(express.json());
 app.use(cors());
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 app.use(
   morgan(
     ":method :url :status :res[content-length] - :response-time ms :header "
   )
 );
 
-let data = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+let data = [];
+
+// Mongoose Connection here.
+const url = process.env.MONGODB_URI;
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(url)
+  .then((res) => {
+    console.log("connected to Database");
+  })
+  .catch((err) => {
+    console.log("error connecting to MongoDB", err.message);
+  });
 
 app.get("/", (req, res) => {
   res.send("<h1>Go to /api/person route</h1>");
 });
 
-app.get("/api/persons", (req, res) => {
-  res.json(data);
+app.get("/api/persons", async (req, res) => {
+  data = await Person.find({});
+  res.status(200).json(data);
 });
 
 app.get("/info", (req, res) => {
   const d = new Date();
-  console.log(d);
   res.send(`<p>Phonebook has info of ${data.length} people</p> <p>${d}</p>`);
 });
 
 app.get("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
-  const person = data.find((person) => person.id === id);
-  res.json(person);
+  Person.find(id).then(person => {
+    res.json(person);
+  })
 });
 
 app.delete("/api/persons/:id", (req, res) => {
@@ -67,30 +63,27 @@ app.delete("/api/persons/:id", (req, res) => {
 });
 
 app.post("/api/persons", (req, res) => {
-  const person = req.body;
-  
-  if (!person.name || person.name.trim() === "" || person.name === undefined) {
+  const body = req.body;
+
+  if (!body.name || body.name.trim() === "" || body.name === undefined) {
     return res.status(400).send({ error: "Name is missing" });
   }
 
-  if (
-    !person.number ||
-    person.number.trim() === "" ||
-    person.number === undefined
-  ) {
+  if (!body.number || body.number.trim() === "" || body.number === undefined) {
     return res.status(400).json({ error: "Number is missing" });
   }
 
-  if (data.some((p) => p.name === person.name)) {
-    return res.status(400).send({ error: "name must be unique" });
-  }
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
 
-  person.id = Math.round(Math.random() * 1000000000);
-  data = data.concat(person);
-  res.json(data);
+  person.save().then((savedPerson) => {
+    res.json(savedPerson);
+  });
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
